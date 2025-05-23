f3682bf3476c4dae5adea85ac2882987daefd91e
nmew14 <nnew14709@gmail.com>
2025-05-23 15:40:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, request
 
-@app.route('/')
-def hello_world():
+app = Flask(name)
+
+@app.route('/', methods=['GET'])
+def hello():
     return 'Hello from Tech VJ'
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+@app.route('/', methods=['POST'])
+def webhook():
+    data = request.get_json()
+    print(data)  # Sirf test ke liye
+    return 'OK', 200
+
+if name == "main":
+    import os
+    port = int(os.environ.get("PORT", 5000))  # Render ka port
+    app.run(host='0.0.0.0', port=port)
